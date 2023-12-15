6a776c2a77e88db8a1293d5d6217c6e77d0fc0c7
pkkhvu <hungkevinvu@gmail.com>
2023-12-15 12:38:36 -0600

    modelling data and enabling uploads // setting up stripe

--- coolhippo/src/config/index.ts
+++ coolhippo/src/config/index.ts
@@ -1,21 +1,21 @@
-const PRODUCT_CATEGORIES = [
+export const PRODUCT_CATEGORIES = [
   {
     label: "UI Kits",
     value: "ui_kits" as const,
     featured: [
       {
         name: "Editor picks",
-        href: "#",
+        href: `/products?category=ui_kits`,
         imageSrc: "/nav/ui-kits/mixed.jpg",
       },
       {
         name: "New Arrivals",
-        href: "#",
+        href: "/products?category=ui_kits&sort=desc",
         imageSrc: "/nav/ui-kits/blue.jpg",
       },
       {
-        name: "Best Sellers",
-        href: "#",
+        name: "Bestsellers",
+        href: "/products?category=ui_kits",
         imageSrc: "/nav/ui-kits/purple.jpg",
       },
     ],
@@ -25,22 +25,20 @@ const PRODUCT_CATEGORIES = [
     value: "icons" as const,
     featured: [
       {
-        name: "Favourite Icon Picks",
-        href: "#",
+        name: "Favorite Icon Picks",
+        href: `/products?category=icons`,
         imageSrc: "/nav/icons/picks.jpg",
       },
       {
         name: "New Arrivals",
-        href: "#",
+        href: "/products?category=icons&sort=desc",
         imageSrc: "/nav/icons/new.jpg",
       },
       {
-        name: "Best Sellers",
-        href: "#",
+        name: "Bestselling Icons",
+        href: "/products?category=icons",
         imageSrc: "/nav/icons/bestsellers.jpg",
       },
     ],
   },
 ];
-
-export default PRODUCT_CATEGORIES;
--- coolhippo/src/payload.config.ts
+++ coolhippo/src/payload.config.ts
@@ -1,18 +1,22 @@
 import { buildConfig } from "payload/config";
+import { webpackBundler } from "@payloadcms/bundler-webpack";
 import { mongooseAdapter } from "@payloadcms/db-mongodb";
 import { slateEditor } from "@payloadcms/richtext-slate";
-import { webpackBundler } from "@payloadcms/bundler-webpack";
 import path from "path";
 import { Users } from "./collections/Users";
 import dotenv from "dotenv";
+import { Products } from "./collections/Products/Products";
+import { Media } from "./collections/Media";
+import { ProductFiles } from "./collections/ProductFile";
+import { Orders } from "./collections/Orders";
 
 dotenv.config({
-  path: path.resolve(__dirname, "../.env.local"),
+  path: path.resolve(__dirname, "../.env"),
 });
 
 export default buildConfig({
   serverURL: process.env.NEXT_PUBLIC_SERVER_URL || "",
-  collections: [Users],
+  collections: [Users, Products, Media, ProductFiles, Orders],
   routes: {
     admin: "/sell",
   },
@@ -20,9 +24,9 @@ export default buildConfig({
     user: "users",
     bundler: webpackBundler(),
     meta: {
-      titleSuffix: "- CoolHippo",
+      titleSuffix: "- DigitalHippo",
       favicon: "/favicon.ico",
-      ogImage: "thumbnail.jpg",
+      ogImage: "/thumbnail.jpg",
     },
   },
   rateLimit: {
